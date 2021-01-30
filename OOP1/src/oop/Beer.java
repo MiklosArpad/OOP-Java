c7e7e595b74959cc86e3c7f6c72a8ce0e1f47655
MiklosAprad <miklosarpad2007@gmail.com>
2021-01-30 16:20:51 +0100

    Konstruktor get-set

---
+++ OOP1/src/oop/Beer.java
@@ -0,0 +1,31 @@
+package oop;
+
+public class Beer {
+    // Adat (példányszintűek: adattag, példányváltozó, attribútum, mező)
+    private String brand;
+
+    // Konstruktor: ő foglal heap-en memóriát, ő konstrukál össze egy objektumot
+
+    public Beer() {
+        // Redundáns, mert én csináltam
+        // Ilyet amúgyis csinál a fordító ...
+        // (Felesleges)
+    }
+
+    public Beer(String brand) {
+        setBrand(brand);
+    }
+
+    // Adaton operáló művelet (példányszintű: metódusok -> eljárás (void), függvény (int, String, Beer, ...))
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getBrandUpperCase() {
+        return brand.toUpperCase();
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
